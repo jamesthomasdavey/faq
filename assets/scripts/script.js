ca901d465250d8e09c41d3d0b7bedbc3245cd896
James Thomas Davey <jamesthomasdavey@Jamess-MacBook-Pro.local>
2018-04-27 16:21:23 -0700

    add styles and giraffe images

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,14 +3,17 @@ const accordionItemEls = document.getElementsByClassName(`accordion__item`);
 function expandDetail() {
   for (let accordionItem of accordionItemEls) {
     let accordionHeader = accordionItem.firstElementChild;
-    accordionHeader.addEventListener(`click`, function() {
+    accordionHeader.addEventListener(`click`, function () {
       let accordionDetails = this.nextElementSibling;
+      let accordionIcon = this.lastElementChild;
       if (accordionDetails.style.maxHeight) {
         accordionDetails.style.maxHeight = null;
-        this.lastElementChild.innerHTML = `+`;
+        accordionIcon.firstElementChild.style.transform = `rotate(-90deg)`;
+        accordionIcon.lastElementChild.style.transform = `rotate(-180deg)`;
       } else {
         accordionDetails.style.maxHeight = accordionDetails.scrollHeight + `px`;
-        this.lastElementChild.innerHTML = `-`;
+        accordionIcon.firstElementChild.style.transform = `rotate(180deg)`;
+        accordionIcon.lastElementChild.style.transform = `rotate(180deg)`;
       }
     })
   }
